e55746dda7709ce22b7a09e2f454b7649ea01f86
Xiaofei <13263397018@163.com>
2020-01-20 15:55:51 +0800

    搭建前后端基本框架

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+// App.js
+import React, { Component } from"react";
+import"./App.css";
+import { connect, sendMsg } from"./api";
+// 从相对路径导入组件
+import Header from'./components/Header/Header';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        connect();
+    }
+
+    send() {
+        console.log("hello");
+        sendMsg("hello");
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <button onClick={this.send}>Hit</button>
+            </div>
+        );
+    }
+}
+
+export default App;
